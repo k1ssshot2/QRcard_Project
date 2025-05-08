126fa03ddea72291cd7864aeee56fd8724227cc2
오금서 <rmatj035@gmail.com>
2025-05-08 22:05:35 +0900

    ..

---
+++ app/src/main/java/com/example/qrcardproject/Friend.java
@@ -0,0 +1,13 @@
+public class Friend {
+    private String name;
+    private String email;
+    private String phone;
+    private String department;
+    private String position;
+    private String kakaoId;
+    private String instagramId;
+
+    // 생성자, getter, setter
+}
+
+
---
+++ app/src/main/java/com/example/qrcardproject/MainActivity.java
@@ -0,0 +1,4 @@
+package com.example.qrcardproject;
+
+public class MainActivity {
+}
