bd148c376da2393eff7a060d066c63831209ced6
오금서 <rmatj035@gmail.com>
2025-06-01 09:23:34 +0900

    Revert "수정"
    This reverts commit 788e4fb2

--- app/src/main/java/com/example/qrcardproject/ContactsFragment.java
+++ app/src/main/java/com/example/qrcardproject/ContactsFragment.java
@@ -21,6 +21,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+
 public class ContactsFragment extends Fragment {
 
     private RecyclerView favoriteRecyclerView;
@@ -48,43 +49,13 @@ public class ContactsFragment extends Fragment {
         favoriteRecyclerView = view.findViewById(R.id.favoriteRecyclerView);
         favoriteRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         favoriteList = new ArrayList<>();
-        favoriteAdapter = new FriendsAdapter(new ArrayList<>(), new FriendsAdapter.OnFriendClickListener() {
-            @Override
-            public void onFriendClick(Friend friend) {
-                onFriendClickImpl(friend);
-            }
-
-            @Override
-            public void onFavoriteClick(Friend friend) {
-
-            }
-
-            @Override
-            public void onFavoriteClick(Friend friend, boolean isNowFavorite) {
-                updateFavoriteInFirestore(friend, isNowFavorite);
-            }
-        });
+        favoriteAdapter = new FriendsAdapter(new ArrayList<>(), this::onFriendClick);
         favoriteRecyclerView.setAdapter(favoriteAdapter);
 
         contactRecyclerView = view.findViewById(R.id.contactRecyclerView);
         contactRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         contactList = new ArrayList<>();
-        contactAdapter = new FriendsAdapter(new ArrayList<>(), new FriendsAdapter.OnFriendClickListener() {
-            @Override
-            public void onFriendClick(Friend friend) {
-                onFriendClickImpl(friend);
-            }
-
-            @Override
-            public void onFavoriteClick(Friend friend) {
-
-            }
-
-            @Override
-            public void onFavoriteClick(Friend friend, boolean isNowFavorite) {
-                updateFavoriteInFirestore(friend, isNowFavorite);
-            }
-        });
+        contactAdapter = new FriendsAdapter(new ArrayList<>(), this::onFriendClick);
         contactRecyclerView.setAdapter(contactAdapter);
 
         db = FirebaseFirestore.getInstance();
@@ -93,14 +64,11 @@ public class ContactsFragment extends Fragment {
         return view;
     }
 
-    private void onFriendClickImpl(Friend friend) {
-        // 친구 객체를 Intent로 전달
-        Intent intent = new Intent(getContext(), FriendProfileActivity.class);
-        intent.putExtra("friend", friend);  // Friend 객체를 Intent에 추가
+    private void onFriendClick(Friend friend) {
+        Intent intent = new Intent(getContext(), FriendProfileFragment.class);
+        intent.putExtra("friend", friend);
         startActivityForResult(intent, REQUEST_VIEW_FRIEND);
     }
-
-
     private void loadFriendsFromFirestore() {
         String currentUserId = FirebaseAuth.getInstance().getCurrentUser().getUid();
         Log.d("ContactsFragment", "현재 사용자 UID: " + currentUserId);
@@ -114,9 +82,13 @@ public class ContactsFragment extends Fragment {
                         favoriteList.clear();
                         contactList.clear();
 
+                        Log.d("ContactsFragment", "Firestore 친구 문서 개수: " + task.getResult().size());
+
                         for (QueryDocumentSnapshot doc : task.getResult()) {
                             Friend friend = doc.toObject(Friend.class);
                             friend.setId(doc.getId());
+
+                            Log.d("ContactsFragment", "가져온 친구: " + friend.getName() + " / 즐겨찾기: " + friend.isFavorite());
 
                             if (friend.isFavorite()) {
                                 favoriteList.add(friend);
@@ -125,6 +97,9 @@ public class ContactsFragment extends Fragment {
                             }
                         }
 
+                        Log.d("ContactsFragment", "즐겨찾기 수: " + favoriteList.size());
+                        Log.d("ContactsFragment", "일반 연락처 수: " + contactList.size());
+
                         favoriteAdapter.setData(groupFriendsWithHeaders(favoriteList));
                         contactAdapter.setData(groupFriendsWithHeaders(contactList));
                     } else {
@@ -132,28 +107,6 @@ public class ContactsFragment extends Fragment {
                     }
                 });
     }
-
-    private void updateFavoriteInFirestore(Friend friend, boolean isNowFavorite) {
-        String currentUserId = FirebaseAuth.getInstance().getCurrentUser().getUid();
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-
-        db.collection("users")
-                .document(currentUserId)
-                .collection("friends")
-                .document(friend.getId())  // 친구 ID로 해당 친구의 문서 참조
-                .update("favorite", isNowFavorite)  // 'favorite' 필드 업데이트
-                .addOnCompleteListener(task -> {
-                    if (task.isSuccessful()) {
-                        // Firestore 업데이트가 성공했을 때
-                        Log.d("ContactsFragment", "즐겨찾기 상태가 성공적으로 업데이트되었습니다.");
-                        // UI 갱신을 위한 코드 추가 (옵션)
-                    } else {
-                        // Firestore 업데이트 실패 시
-                        Log.w("ContactsFragment", "즐겨찾기 상태 업데이트 실패", task.getException());
-                    }
-                });
-    }
-
 
 
 
@@ -170,7 +123,7 @@ public class ContactsFragment extends Fragment {
             if (name != null && !name.isEmpty()) {
                 header = name.substring(0, 1).toUpperCase();
             } else {
-                header = "?";
+                header = "?";  // 이름이 비어 있거나 null이면 '?' 섹션으로
             }
 
             if (!header.equals(lastHeader)) {
@@ -183,6 +136,7 @@ public class ContactsFragment extends Fragment {
 
         return groupedList;
     }
+
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
@@ -215,8 +169,12 @@ public class ContactsFragment extends Fragment {
             }
         }
 
-        favoriteAdapter.setData(groupFriendsWithHeaders(favoriteList));
-        contactAdapter.setData(groupFriendsWithHeaders(contactList));
+        List<FriendListItem> groupedFavoriteList = groupFriendsWithHeaders(favoriteList);
+        List<FriendListItem> groupedContactList = groupFriendsWithHeaders(contactList);
+
+        favoriteAdapter.setData(groupedFavoriteList);
+        contactAdapter.setData(groupedContactList);
     }
 }
+
 
